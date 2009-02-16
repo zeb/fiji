893cd946f3da1f281f4bf5e95fa1527c8eb170ee
Mark Longair <Mark.Longair@ed.ac.uk>
2009-02-16 15:53:50 +0000

    Harcode the location of the icon file in the launcher

--- fiji.cxx
+++ fiji.cxx
@@ -1585,7 +1585,7 @@ static int start_ij(void)
 
 		update_files();
 		stringstream icon_option;
-		icon_option << "-icon=" << fiji_dir << "/images/icon.png";
+		icon_option << "-icon=/usr/share/fiji/images/icon.png";
 		add_option(options, icon_option, 1);
 		add_option(options, "-title=Fiji", 1);
 	}
