6acf8bbec9eb6f6149ccfe7345afec2177b837ec
Johannes Schindelin <johannes.schindelin@gmx.de>
2009-05-28 19:43:28 +0200

    WIP

--- src-plugins/JMathLib_Interpreter/Refresh_JMathLib_Scripts.java
+++ src-plugins/JMathLib_Interpreter/Refresh_JMathLib_Scripts.java
@@ -1,101 +1,58 @@
-
-package JMathLib_Interpreter;
-
 import ij.IJ;
 
 import common.RefreshScripts;
+
+import java.io.File;
+
+import jmathlib.core.functions.FileFunctionLoader;
+import jmathlib.core.functions.WebFunctionLoader;
+
 import jmathlib.core.interpreter.Interpreter;
 import jmathlib.core.interfaces.JMathLibOutput;
 
-import java.io.*;
-
 public class Refresh_JMathLib_Scripts extends RefreshScripts 
 			implements JMathLibOutput {
-	
-	private DataInputStream input;
-	private boolean interactiveMode = true;
-	private String functionCode = "";
-	private boolean exiting = false ;
 	Interpreter interpreter;
 	
-	final protected ByteArrayOutputStream byte_out = new ByteArrayOutputStream();
-	final protected BufferedOutputStream out = new BufferedOutputStream(byte_out);
-	final protected PrintWriter print_out = new PrintWriter(out);
-
 	public void run(String arg) {
-		setLanguageProperties(".m","Matlab");
+		setLanguageProperties(".m", "Matlab");
 		setVerbose(false);
 		super.run(arg);
 	}
 
 	public void runScript(String filename) {
-		interpreter = new Interpreter(true);
+		interpreter = new Interpreter(false);
 		interpreter.setOutputPanel(this);
-		//interpreter.executeExpression("startup");		
-		input = new DataInputStream(filename);
-		displayPrompt();
-        	while(!exiting) {
-       		
-            		String command = readLine();
-           		interpretLine(command);    
-        	}
-
+interpreter.executeExpression("cos(1)");
+		File file = new File(filename);
+/*
+		interpreter.globals.getFunctionManager().addFunctionLoader(new
+			FunctionLoader(file.getParentFile());
+*/
+		String name = file.getName();
+		if (name.endsWith(".m"))
+			name = name.substring(0, name.length() - 2);
+System.err.println("executing " + name + "();");
+		interpreter.executeExpression(name + "()");
 	}
-
-		
-
-	private String readLine() {
-        	try {
-        		return input.readLine();
-        	}
-        	catch(IOException error) {
-        		System.out.println("IO Exception");
-        	}
-        	return "";
-    	}
-
-	public void interpretLine(String line) {
-    		if(interactiveMode) {    
-			//check to see if this is the beginning of a user function
-        		if(line.length() > 7 && line.substring(0, 8).equalsIgnoreCase("FUNCTION")) {
-        			functionCode = line;
-        			interactiveMode = false;	
-		
-				displayPrompt();
-        		}
-        		else {
-                    		String answerString = interpreter.executeExpression(line);
-            
-                    		displayText(answerString);
-                    		displayPrompt();		        
-	        	}
-	   	}
-	   	else {   		
-	   		if(line.equalsIgnoreCase("END")) {
-		   		String answerString = "";
-	   			//process the function
-	   			//answerString = interpreter.readFunction(functionCode, true, false);
-	   			
-	   			interactiveMode = true;
-
-		        	displayText(answerString);
-	   		}		    
-	   		else
-		   		functionCode += line;
-	   		
-	        displayPrompt();
-	   	}
-    	}
 	
 	public void displayText(String text) {
-		print_out.println(text);
+		IJ.write(text);
 	}
 
 	public void displayPrompt() {
-        	print_out.print("> ");
+		IJ.write("> ");
     	}
 
 	public void setStatusText(String text) {
-		print_out.println(text);
+		IJ.showStatus(text);
+	}
+
+	class FunctionLoader extends WebFunctionLoader {
+		FileFunctionLoader realLoader;
+
+		public FunctionLoader(File directory) {
+			super();
+		}
 	}
 }
