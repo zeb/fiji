de22a5ec8573311b079af955948c4843656cd0a1
Stephan Saalfeld <saalfeld@mpi-cbg.de>
2009-06-11 16:30:16 +0200

    Adding triangles as annotations.

--- src-plugins/janelia_/DMesh_Visualization.java
+++ src-plugins/janelia_/DMesh_Visualization.java
@@ -1,5 +1,6 @@
 import janelia.dmesh.DMesh;
 
+import java.awt.geom.Area;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -9,6 +10,9 @@ import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Scanner;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 
 import loci.formats.FormatException;
 import loci.formats.IFormatReader;
@@ -32,6 +36,10 @@ import ini.trakem2.display.Layer;
 import ini.trakem2.display.Patch;
 import ini.trakem2.io.AmiraImporter;
 import ini.trakem2.persistence.FSLoader;
+import ini.trakem2.tree.ProjectThing;
+import ini.trakem2.tree.ProjectTree;
+import ini.trakem2.tree.TemplateThing;
+import ini.trakem2.tree.TemplateTree;
 import ini.trakem2.utils.Utils;
 
 
@@ -137,7 +145,10 @@ public class DMesh_Visualization implements PlugIn
 				IJ.log( "saved file " + dir.getAbsolutePath() + "/dmeshmapping.bsh" );
 				
 				final Project project = Project.newFSProject( "blank", null, dir.getAbsolutePath() );
-				final Layer layer = project.getRootLayerSet().getLayer( 0 );
+				final Layer layer1 = project.getRootLayerSet().getLayer( 0 );
+				final Layer layer2 = project.getRootLayerSet().getLayer( 40, 40, true );
+				
+				project.setProperty( "no_shutdown_hook", "true" );
 				
 				/* translating compressed tiff files into something ImageJ can eat */
 				IJ.save( openImageFile( dir.getAbsolutePath() + "/1.tif" ), dir.getAbsolutePath() + "/1.ij.tif" );
@@ -145,10 +156,59 @@ public class DMesh_Visualization implements PlugIn
 				final Patch p1 = project.getLoader().importImage( project, 0, 0, dir.getAbsolutePath() + "/1.ij.tif" );
 				final Patch p2 = project.getLoader().importImage( project, 0, 0, dir.getAbsolutePath() + "/2.ij.tif" );
 				p2.setPreprocessorScriptPath( dir.getAbsolutePath() + "/dmeshmapping.bsh" );
-				layer.add( p1 );
-				layer.add( p2 );
+				layer1.add( p1 );
+				layer2.add( p2 );
 				
-				final Map< Float, AreaList > triangles = AmiraImporter.extractAreaLists( openImageFile( dir.getAbsolutePath() + "/map.tif" ), layer, 0, 0, 0.5f, false );
+				final Map< Float, AreaList > triangles = AmiraImporter.extractAreaLists( openImageFile( dir.getAbsolutePath() + "/map.tif" ), layer1, 0, 0, 0.2f, false );
+				
+				final TemplateTree ttree = project.getTemplateTree();  // these are DNDTree which extend JTree
+				final ProjectTree ptree = project.getProjectTree();
+
+
+				TemplateThing troot = ( TemplateThing ) ( ( DefaultMutableTreeNode ) ttree.getModel().getRoot() ).getUserObject();
+
+				TemplateThing tt1 = ttree.addNewChildType( troot, "dmesh" );
+				TemplateThing tt2 = ttree.addNewChildType( tt1, "triangle" );
+				TemplateThing tt3 = ttree.addNewChildType( tt2, "area_list" );
+				
+				ProjectThing proot = project.getRootProjectThing();
+				DefaultTreeModel pmodel = ( DefaultTreeModel ) ptree.getModel();
+				DefaultMutableTreeNode proot_node = ( DefaultMutableTreeNode ) ptree.getRoot();
+				// proot == root_node.getUserObject()
+
+				ProjectThing pt0 = proot.createChild( "anything" );
+				DefaultMutableTreeNode node0 = new DefaultMutableTreeNode( pt0 );
+				proot_node.add( node0 );
+				
+				// Insert a new "transformations" node instance into the root
+				// node of the ProjectTree
+				ProjectThing pt1 = pt0.createChild( "dmesh" );
+				DefaultMutableTreeNode node1 = new DefaultMutableTreeNode( pt1 );
+				
+				for ( final Map.Entry<  Float, AreaList > e : triangles.entrySet() )
+				{
+					/* Insert a new "triangle" node instance into the pt1 node; */
+					final ProjectThing pt2 = pt1.createChild( "triangle" );
+					DefaultMutableTreeNode node2 = new DefaultMutableTreeNode( pt2 );
+					node1.add( node2 ); // at index 0
+	
+					// Insert a new "arealist" node instance into the pt2 node
+					final AreaList al1 = e.getValue();
+					al1.setArea( layer2.getId(), new Area( al1.getArea( layer1 ) ) );
+					ProjectThing pt3 = new ProjectThing( tt3, project, e.getValue() );
+					pt2.addChild( pt3 );
+					e.getValue().setTitle( "" + ( int )( e.getKey() + 0.5f ) );
+					
+					DefaultMutableTreeNode node3 = new DefaultMutableTreeNode( pt3 );
+					node2.add( node3 );
+				}
+				
+				/* finally adding the root nopde into the tree */
+				node0.add( node1 );
+				
+				// repaint
+				ttree.updateUILater();
+				ptree.updateUILater();
 				
 				Display.repaint();
 								
