2aa7e1b2dfb1296408a019d40d77087dfc16e349
Andreas Wiese <wiese@mpi-cbg.de>
2009-09-08 20:57:22 +0200

    Test clean-up

--- tests/fiji-main/component_path.rb
+++ tests/fiji-main/component_path.rb
@@ -2,7 +2,7 @@
 #
 
 require 'java'
-require '../testlib'
+require '../testlib' # Container.getAllComponents
 
 include_class 'fiji.Main'
 include_class 'java.awt.Button'
@@ -10,35 +10,37 @@ include_class 'java.awt.Label'
 include_class 'java.awt.Panel'
 include_class 'java.awt.TextField'
 
-class ComponentPathDialog < Java::JavaAwt::Dialog
-  def initialize
-    super(nil, "ComponentPathTest")
+def color(bool)
+  color = bool ? 32 : 31
 
-    @panel = Panel.new
-    @panel.add(Label.new('Hello'))
-    @panel.add(Button.new('Hello'))
-    @panel.add(Button.new('Hello'))
-    @panel.add(TextField.new('Bla'))
-    @panel.add(Label.new('Hello'))
-    @panel.add(Button.new('Hello'))
-
-    self.add(@panel)
-  end
+  return "\x1b[#{color};01m#{bool}\x1b[m"
 end
 
-if $0 == __FILE__
-  Main.premain
+Main.premain
 
-  cdialog = ComponentPathDialog.new
-  cdialog.pack
-  cdialog.setVisible(true)
+cdialog = Java::JavaAwt::Dialog.new(nil, "ComponentPathTest")
+panel = Panel.new
+panel.add(Label.new('Hello'))
+panel.add(Button.new('Hello'))
+panel.add(Button.new('Hello'))
+panel.add(TextField.new('Bla'))
+panel.add(Label.new('Hello'))
+panel.add(Button.new('Hello'))
+cdialog.add(panel)
 
-  cdialog.getAllComponents.each do |co|
-    path = Main.getPath(co)
-    result = Main.getComponent(path) == co
+cdialog.pack
+cdialog.setVisible(true)
 
-    puts "co = #{co}"
-    puts "  path = #{path}"
-    puts "  getComponent(getPath(co)) == co: #{result}"
-  end
+cdialog.getAllComponents.each do |co|
+
+  path = Main.getPath(co)
+  result = Main.getComponent(path) == co
+
+  puts "co = #{co}"
+  puts "  path = #{path}"
+  puts "  getComponent(getPath(co)) == co: #{color(result)}"
+  puts
 end
+
+cdialog.setVisible(false)
+Java::JavaLang::System.exit(0)
