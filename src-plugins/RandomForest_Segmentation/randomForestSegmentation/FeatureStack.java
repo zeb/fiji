b3f2b16caee5c11da74ff8f99565f4f8c421c5da
Verena Kaynig <verena.kaynig@inf.ethz.ch>
2009-09-11 11:39:22 +0200

    Add the plugin "RandomForest Segmentation"
    Signed-off-by: Verena Kaynig <verena.kaynig@inf.ethz.ch>

---
+++ src-plugins/RandomForest_Segmentation/randomForestSegmentation/FeatureStack.java
@@ -0,0 +1,259 @@
+package randomForestSegmentation;
+/* This class is intended for the Simple_Segmentation plugin. It creates and holds
+ * different feature images for the classification. Possible filters include:
+ * - Gaussianblur
+ * - Gradientmagnitude
+ * - Hessian
+ * - Difference of Gaussian
+ * 
+ * License: GPL
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Author: Verena Kaynig (verena.kaynig@inf.ethz.ch)
+ */
+
+import weka.core.Attribute;
+import weka.core.FastVector;
+import weka.core.Instance;
+import weka.core.Instances;
+import ij.IJ;
+import ij.ImageStack;
+import ij.ImagePlus;
+import ij.gui.Roi;
+import ij.process.ByteProcessor;
+import ij.process.FHT;
+import ij.process.FloatPolygon;
+import ij.process.FloatProcessor;
+import ij.process.ImageProcessor;
+import ij.plugin.ZProjector;
+import ij.plugin.filter.GaussianBlur;
+import ij.plugin.filter.Convolver;
+
+public class FeatureStack {
+	private ImagePlus originalImage;
+	private ImageStack wholeStack;
+	private int width;
+	private int height;
+	
+	public FeatureStack(ImagePlus image){
+		originalImage = image;
+		width = image.getWidth();
+		height = image.getHeight();
+		wholeStack = new ImageStack(width, height);
+		wholeStack.addSlice("original", image.getProcessor().duplicate());
+	}
+
+	public void show(){
+		ImagePlus showStack = new ImagePlus("featureStack", wholeStack);
+		showStack.show();
+	}
+	
+	public int getSize(){
+		return wholeStack.getSize();
+	}
+	
+	public String getSliceLabel(int index){
+		return wholeStack.getSliceLabel(index);
+	}
+	
+	public int getHeight(){
+		return wholeStack.getHeight();
+	}
+	
+	public int getWidth(){
+		return wholeStack.getWidth();
+	}
+	
+	public void addGaussianBlur(float sigma){
+		ImageProcessor ip = originalImage.getProcessor().duplicate();
+		GaussianBlur gs = new GaussianBlur();
+		gs.blur(ip, sigma);
+		wholeStack.addSlice("GaussianBlur_"+sigma, ip);
+	}
+	
+	public void addGradient(float sigma){
+		GaussianBlur gs = new GaussianBlur();
+		ImageProcessor ip_x = originalImage.getProcessor().duplicate().convertToFloat();
+		gs.blur(ip_x, sigma);
+		Convolver c = new Convolver();
+		float[] sobelFilter_x = {1f,2f,1f,0f,0f,0f,-1f,-2f,-1f};
+		c.convolveFloat(ip_x, sobelFilter_x, 3, 3);
+		
+		ImageProcessor ip_y = originalImage.getProcessor().duplicate().convertToFloat();
+		gs.blur(ip_y, sigma);
+		c = new Convolver();
+		float[] sobelFilter_y = {1f,0f,-1f,2f,0f,-2f,1f,0f,-1f};
+		c.convolveFloat(ip_y, sobelFilter_y, 3, 3);
+		
+		ImageProcessor ip = new FloatProcessor(width, height);
+		
+		for (int x=0; x<width; x++){
+			for (int y=0; y<height; y++){
+				float s_x = ip_x.getf(x,y);
+				float s_y = ip_y.getf(x,y);
+				ip.setf(x,y, (float) Math.sqrt(s_x*s_x + s_y*s_y));
+			}
+		}
+		
+		ip.add(-ip.getMin());
+		wholeStack.addSlice("SobelFilter_"+sigma, ip.convertToByte(true));
+	}
+	
+	public void addHessian(float sigma){
+		float[] sobelFilter_x = {1f,2f,1f,0f,0f,0f,-1f,-2f,-1f};
+		float[] sobelFilter_y = {1f,0f,-1f,2f,0f,-2f,1f,0f,-1f};
+		Convolver c = new Convolver();				
+		GaussianBlur gs = new GaussianBlur();
+		
+		ImageProcessor ip_x = originalImage.getProcessor().duplicate().convertToFloat();
+		gs.blur(ip_x, sigma);		
+		c.convolveFloat(ip_x, sobelFilter_x, 3, 3);		
+		
+		ImageProcessor ip_y = originalImage.getProcessor().duplicate().convertToFloat();
+		gs.blur(ip_y, sigma);
+		c = new Convolver();
+		c.convolveFloat(ip_y, sobelFilter_y, 3, 3);
+		
+		ImageProcessor ip_xx = ip_x.duplicate().convertToFloat();
+		//gs.blur(ip_xx, sigma);		
+		c.convolveFloat(ip_xx, sobelFilter_x, 3, 3);		
+		
+		ImageProcessor ip_xy = ip_x.duplicate().convertToFloat();
+		//gs.blur(ip_xy, sigma);		
+		c.convolveFloat(ip_xy, sobelFilter_y, 3, 3);		
+		
+		ImageProcessor ip_yy = ip_y.duplicate().convertToFloat();
+		//gs.blur(ip_yy, sigma);		
+		c.convolveFloat(ip_yy, sobelFilter_y, 3, 3);		
+		
+		ImageProcessor ip = new FloatProcessor(width, height);
+		ImageProcessor ipTr = new FloatProcessor(width, height);
+		ImageProcessor ipDet = new FloatProcessor(width, height);
+				
+		for (int x=0; x<width; x++){
+			for (int y=0; y<height; y++){
+				float s_xx = ip_xx.getf(x,y);
+				float s_xy = ip_xy.getf(x,y);
+				float s_yy = ip_yy.getf(x,y);
+				ip.setf(x,y, (float) Math.sqrt(s_xx*s_xx + s_xy*s_xy+ s_yy*s_yy));
+				ipTr.setf(x,y, (float) s_xx + s_yy);
+				ipDet.setf(x,y, (float) s_xx*s_yy-s_xy*s_xy);
+			}
+		}
+		
+		ip.add(-ip.getMin());
+		ipTr.add(-ipTr.getMin());
+		ipDet.add(-ipDet.getMin());
+		wholeStack.addSlice("Hessian"+sigma, ip.convertToByte(true));
+		wholeStack.addSlice("HessianTrace"+sigma, ipTr.convertToByte(true));
+		wholeStack.addSlice("HessianDeterminant"+sigma, ipDet.convertToByte(true));
+	}
+	
+	public void addDoG(float sigma1, float sigma2){
+		GaussianBlur gs = new GaussianBlur();
+		ImageProcessor ip_1 = originalImage.getProcessor().duplicate();
+		gs.blur(ip_1, sigma1);
+		ImageProcessor ip_2 = originalImage.getProcessor().duplicate();
+		gs.blur(ip_2, sigma2);
+		
+		ImageProcessor ip = new FloatProcessor(width, height);
+		
+		for (int x=0; x<width; x++){
+			for (int y=0; y<height; y++){
+				float v1 = ip_1.getf(x,y);
+				float v2 = ip_2.getf(x,y);
+				ip.setf(x,y, v2-v1);
+			}
+		}
+		
+		ip.add(-ip.getMin());
+		wholeStack.addSlice("DoG_"+sigma1+"_"+sigma2, ip.convertToByte(true));
+	}
+	
+	public void addMembraneFeatures(int patchSize, int membraneSize){
+		//create membrane patch
+		ImageProcessor membranePatch = new FloatProcessor(patchSize, patchSize);
+		int middle = Math.round(patchSize / 2);
+		int startX = middle - (int) Math.floor(membraneSize/2.0);
+		int endX = middle + (int) Math.ceil(membraneSize/2.0);
+		
+		for (int x=startX; x<=endX; x++){
+			for (int y=0; y<patchSize; y++){
+				membranePatch.setf(x, y, 1f);
+			}
+		}
+
+		
+		ImageStack is = new ImageStack(width, height);
+		ImageProcessor rotatedPatch;
+		for (int i=0; i<18; i++){
+			rotatedPatch = membranePatch.duplicate();
+			rotatedPatch.invert();rotatedPatch.rotate(10*i);rotatedPatch.invert();
+			Convolver c = new Convolver();				
+	
+			float[] kernel = (float[]) membranePatch.getPixels();
+			ImageProcessor ip = originalImage.getProcessor().duplicate().convertToFloat();		
+			c.convolveFloat(ip, kernel, patchSize, patchSize);		
+
+			is.addSlice("Membrane_"+patchSize+"_"+membraneSize, ip.convertToByte(true));
+		//	wholeStack.addSlice("Membrane_"+patchSize+"_"+membraneSize, ip.convertToByte(true));
+		}
+		
+		ImagePlus projectStack = new ImagePlus("membraneStack",is);
+		ZProjector zp = new ZProjector(projectStack);
+		zp.setStopSlice(is.getSize());
+		for (int i=0;i<6; i++){
+			zp.setMethod(i);
+			zp.doProjection();
+			wholeStack.addSlice("Membrane_Projection_"+i+"_"+patchSize+"_"+membraneSize, zp.getProjection().getChannelProcessor().convertToByte(true));
+		}
+	}
+	
+	public ImageProcessor getProcessor(int index) {
+		return wholeStack.getProcessor(index);
+	}
+	
+	public Instances createInstances(){
+		FastVector attributes = new FastVector();
+		for (int i=1; i<=wholeStack.getSize(); i++){
+			String attString = wholeStack.getSliceLabel(i) + " numeric";
+			attributes.addElement(new Attribute(attString));
+		}
+		FastVector classes = new FastVector();
+		classes.addElement("foreground");
+		classes.addElement("background");
+		attributes.addElement(new Attribute("class", classes));
+		
+		Instances data =  new Instances("segment", attributes, width*height);
+		
+		Object[] pixelData = wholeStack.getImageArray();
+		
+		for (int y=0; y<wholeStack.getHeight(); y++){
+			IJ.showProgress(y, wholeStack.getHeight());
+			for (int x=0; x<wholeStack.getWidth(); x++){
+				double[] values = new double[wholeStack.getSize()+1];
+				for (int z=1; z<=wholeStack.getSize(); z++){
+					//values[z-1] = wholeStack.getProcessor(z).getPixelValue(x, y);
+					values[z-1] = 0xff & ((byte[]) pixelData[z-1])[y*width+x];
+					//System.out.println("" + wholeStack.getProcessor(z).getPixelValue(x, y) + " * " + values[z-1]);
+				}
+				values[wholeStack.getSize()] = 0.0;
+				data.add(new Instance(1.0, values));
+			}
+		}
+		
+		return data;
+	}
+}
---
+++ src-plugins/RandomForest_Segmentation/randomForestSegmentation/RandomForest_Segmentation.java
@@ -0,0 +1,316 @@
+package randomForestSegmentation;
+/* This is a small Plugin that should perform better in segmentation than thresholding
+ * The idea is to train a random forest classifier on given manual labels
+ * and then classify the whole image 
+ * I try to keep parameters hidden from the user to make usage of the plugin
+ * intuitive and easy. I decided that it is better to need more manual annotations
+ * for training and do feature selection instead of having the user manually tune 
+ * all filters.
+ * 
+ * ToDos:
+ * - work on whole Stack and distinguish between train and test images
+ *     - Another option is to divide the image into train and test area
+ * - give option to specify annotations as test data and give confusion matrix
+ * - give option to delete annotations
+ * - save classifier and load classifier
+ * - apply classifier to other images
+ * - put thread solution to wiki http://pacific.mpi-cbg.de/wiki/index.php/Developing_Fiji#Writing_plugins
+ * 
+ * License: GPL
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Author: Verena Kaynig (verena.kaynig@inf.ethz.ch)
+ */
+
+
+import ij.IJ;
+import ij.plugin.PlugIn;
+import ij.process.FloatProcessor;
+import ij.process.ImageProcessor;
+import ij.gui.ImageWindow;
+import ij.gui.Roi;
+import ij.ImagePlus;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+import weka.core.Attribute;
+import weka.core.FastVector;
+import weka.core.Instance;
+import weka.core.Instances;
+import hr.irb.fastRandomForest.FastRandomForest;
+
+public class RandomForest_Segmentation implements PlugIn {
+	
+	private List<Roi> positiveExamples = new ArrayList< Roi>(); 
+	private List<Roi> negativeExamples = new ArrayList< Roi>();
+	private ImagePlus trainingImage;
+	private ImagePlus displayImage;
+	private FeatureStack featureStack;
+   	final Button posExampleButton;
+  	final Button negExampleButton;
+  	final Button trainButton;
+ 
+  	
+  	public RandomForest_Segmentation() {
+	    	posExampleButton = new Button("positiveExample");
+  	      	negExampleButton = new Button("negativeExample");
+  	      	trainButton = new Button("train Classifier");
+	}
+	
+  	ExecutorService exec = Executors.newFixedThreadPool(1);
+  	
+  	private ActionListener listener = new ActionListener() {
+  		public void actionPerformed(final ActionEvent e) {
+  			exec.submit(new Runnable() {
+  				public void run() {
+  					if(e.getSource() == posExampleButton){
+  		  				addPositiveExamples();
+  		  			}
+  		  			if(e.getSource() == negExampleButton){
+  		  				addNegativeExamples();
+  		  			}
+  		  			if(e.getSource() == trainButton){
+  		  				trainClassifier();
+  		  			}
+  				}
+  			});
+  		}
+  	};
+  	
+ 
+  	
+  	private class CustomWindow extends ImageWindow {
+  		CustomWindow(ImagePlus imp) {
+  			super(imp);
+  			Panel all = new Panel();
+  			BoxLayout box = new BoxLayout(all, BoxLayout.Y_AXIS);
+  			all.setLayout(box);
+  			
+  			Panel piw = new Panel();
+  			piw.setLayout(super.getLayout());
+  			setTitle("Playground");
+  			for (Component c : getComponents()) {
+  				piw.add(c);
+  			}
+  			
+  			
+  			Panel buttons = new Panel();
+  			buttons.setMaximumSize(new Dimension(Integer.MAX_VALUE, 40));
+  			
+  	      	posExampleButton.addActionListener(listener);
+  	      	negExampleButton.addActionListener(listener);
+  	      	trainButton.addActionListener(listener);
+  	      	buttons.add(posExampleButton);
+  	      	buttons.add(negExampleButton);
+  	      	buttons.add(trainButton);
+  	      	
+  	      	for (Component c : new Component[]{posExampleButton, negExampleButton, trainButton}) {
+  	      		c.setMaximumSize(new Dimension(230, 50));
+  	      		c.setPreferredSize(new Dimension(130, 30));
+  	      	}
+  	      	
+  	      	all.add(piw);
+  	      	all.add(buttons);
+  	      	
+  	      	removeAll();
+  	      	
+  	      	add(all);
+  	      	
+  	      	pack();
+  	      	
+  	      	// Propagate all listeners
+  	      	for (Panel p : new Panel[]{all, buttons, piw}) {
+  	      		for (KeyListener kl : getKeyListeners()) {
+  	      			p.addKeyListener(kl);
+  	      		}
+  	      	}
+  	      	
+  	      	addWindowListener(new WindowAdapter() {
+  	      		public void windowClosing(WindowEvent e) {
+  	      			// cleanup
+  	      			exec.shutdownNow();
+  	      		}
+  	      	});
+  		}
+  	}
+  	
+	public void run(String arg) {
+		//get current image
+//		trainingImage = WindowManager.getCurrentImage();
+//		trainingImage = IJ.openImage("testImages/i00000-1.tif");
+		trainingImage = IJ.openImage();
+		createFeatureStack(trainingImage);
+		displayImage = new ImagePlus();
+		displayImage.setProcessor("training image", trainingImage.getProcessor().convertToRGB());
+		trainingImage.setProcessor("training image", trainingImage.getProcessor().convertToByte(true));
+		
+		//Build GUI
+		ImageWindow win = new CustomWindow(displayImage);
+		}
+	
+	private void addPositiveExamples(){
+		//get selected pixels
+		Roi r = displayImage.getRoi();
+		displayImage.setColor(Color.GREEN);
+		displayImage.killRoi();
+		r.drawPixels(displayImage.getProcessor());
+		displayImage.updateAndDraw();
+		positiveExamples.add(r);
+	}
+	
+	private void addNegativeExamples(){
+		//get selected pixels
+		Roi r = displayImage.getRoi();
+		displayImage.setColor(Color.RED);
+		displayImage.killRoi();
+		r.drawPixels(displayImage.getProcessor());
+		displayImage.updateAndDraw();
+		negativeExamples.add(r);
+	}
+	
+	public void createFeatureStack(ImagePlus img){
+		IJ.log("creating feature stack");
+		featureStack = new FeatureStack(img);
+		for (float i=2.0f; i<featureStack.getWidth()/5.0f; i*=2){
+			featureStack.addGaussianBlur(i);
+			featureStack.addGradient(i);
+			featureStack.addHessian(i);
+			for (float j=2.0f; j<i; j*=2){
+				featureStack.addDoG(i, j);
+			}
+		}
+		
+//		featureStack.addMembraneFeatures(11, 1);
+//		featureStack.addMembraneFeatures(31, 3);
+//		featureStack.addMembraneFeatures(51, 3);
+//		featureStack.show();
+	}
+	
+	
+	public void writeDataToARFF(Instances data, String filename){
+		try{
+			BufferedWriter out = new BufferedWriter(
+				new OutputStreamWriter(
+				new FileOutputStream( filename) ) );
+			try{	
+					out.write(data.toString());
+					
+			}
+			catch(IOException e){IJ.showMessage("IOException");}
+	}
+	catch(FileNotFoundException e){IJ.showMessage("File not found!");}
+		
+	}
+
+	public Instances createTrainingInstances(){
+		FastVector attributes = new FastVector();
+		for (int i=1; i<=featureStack.getSize(); i++){
+			String attString = featureStack.getSliceLabel(i) + " numeric";
+			attributes.addElement(new Attribute(attString));
+		}
+		FastVector classes = new FastVector();
+		classes.addElement("foreground");
+		classes.addElement("background");
+		attributes.addElement(new Attribute("class", classes));
+		
+		Instances trainingData =  new Instances("segment", attributes, positiveExamples.size()+negativeExamples.size());
+		
+		for(int j=0; j<positiveExamples.size(); j++){
+			Roi r = positiveExamples.get(j);
+			int[] x = r.getPolygon().xpoints;
+			int[] y = r.getPolygon().ypoints;
+			int n = r.getPolygon().npoints;
+			
+			for (int i=0; i<n; i++){
+				double[] values = new double[featureStack.getSize()+1];
+				for (int z=1; z<=featureStack.getSize(); z++){
+					values[z-1] = featureStack.getProcessor(z).getPixelValue(x[i], y[i]);
+				}
+				values[featureStack.getSize()] = 1.0;
+				trainingData.add(new Instance(1.0, values));
+			}
+		}
+		
+		for(int j=0; j<negativeExamples.size(); j++){
+			Roi r = negativeExamples.get(j);
+			int[] x = r.getPolygon().xpoints;
+			int[] y = r.getPolygon().ypoints;
+			int n = r.getPolygon().npoints;
+			
+			for (int i=0; i<n; i++){
+				double[] values = new double[featureStack.getSize()+1];
+				for (int z=1; z<=featureStack.getSize(); z++){
+					values[z-1] = featureStack.getProcessor(z).getPixelValue(x[i], y[i]);
+				}
+				values[featureStack.getSize()] = 0.0;
+				trainingData.add(new Instance(1.0, values));
+			}
+		}
+		return trainingData;
+	}
+	
+	public void trainClassifier(){
+		 long start = System.currentTimeMillis();
+		 Instances data = createTrainingInstances();
+		 long end = System.currentTimeMillis();
+		 IJ.log("creating training data took: " + (end-start));
+		 data.setClassIndex(data.numAttributes() - 1);
+		 writeDataToARFF(data, "trainingDataFromInstances.arff");
+		 
+		 FastRandomForest rf = new FastRandomForest();
+		 rf.setNumTrees(50);
+		 //this is the default that Breiman suggests
+		 rf.setNumFeatures((int) Math.round(Math.sqrt(featureStack.getSize())));
+		 //rf.setNumFeatures(2);
+		 rf.setSeed(123);
+		 
+		 IJ.log("training classifier");
+		 try{rf.buildClassifier(data);}
+		 catch(Exception e){IJ.showMessage("Could not train Classifier!");}
+		 
+		 IJ.log("reading whole image data");
+		 start = System.currentTimeMillis();
+		 data = featureStack.createInstances();
+		 end = System.currentTimeMillis();
+		 IJ.log("creating whole image data took: " + (end-start));
+		 data.setClassIndex(data.numAttributes() - 1);
+		 
+		 writeDataToARFF(data, "testWholeImageData.arff");
+		 
+		 IJ.log("classifying image");
+		 double[] classificationResult = new double[data.numInstances()];
+		 for (int i=0; i<data.numInstances(); i++){
+			 try{
+			 classificationResult[i] = rf.classifyInstance(data.instance(i));
+			 }catch(Exception e){IJ.showMessage("Could not apply Classifier!");}
+		 }
+		 
+		 IJ.log("showing result");
+		 ImageProcessor classifiedImageProcessor = new FloatProcessor(trainingImage.getWidth(), trainingImage.getHeight(), classificationResult);
+		 ImagePlus classifiedImage = new ImagePlus("classification result", classifiedImageProcessor);
+		 classifiedImage.show();
+	}
+
+
+	
+}
