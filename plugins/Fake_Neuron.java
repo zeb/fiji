505480f58c44e42f65cbb8d18f102737473c75c4
Andreas Wiese <wiese@mpi-cbg.de>
2009-09-30 12:22:23 +0200

    Further faking

--- plugins/Fake_Neuron.java
+++ plugins/Fake_Neuron.java
@@ -5,6 +5,8 @@ import ij.gui.GenericDialog;
 import ij.plugin.PlugIn;
 import ij.process.ByteProcessor;
 import ij.process.ImageProcessor;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Fake_Neuron
 	extends ImagePlus
@@ -13,6 +15,14 @@ public class Fake_Neuron
 	private int w = 320, h = 320, d = 64;
 	private String title = "Fake Neuron";
 	private byte[][] pixels;
+
+	private List<int[]> pixellist;
+
+	public Fake_Neuron()
+	{
+		super();
+		this.pixellist = new ArrayList<int[]>();
+	}
 
 	public void run(String args)
 	{
@@ -46,17 +56,73 @@ public class Fake_Neuron
 
 		this.setStack(title, stack);
 
-		paintBall((int)(Math.random() * x),
-				(int)(Math.random() * y),
-				(int)(Math.random() * z), 10);
+		paintNeuron(null, null);
 
 		this.show();
 
 		return;
 	}
 
-	private void paintBall(int x, int y, int z, int r)
+	private void paintNeuron(int a[], int z[])
 	{
+		int m[];
+		double w = (double)this.getWidth();
+		double h = (double)this.getHeight();
+		double d = (double)this.getNSlices();
+
+
+		if (a == null) {
+			a = new int[4];
+			a[0] = (int)(w * 0.1 * Math.random());
+			a[1] = (int)(h * 0.1 * Math.random());
+			a[2] = (int)(d * 0.1 * Math.random());
+			a[3] = 5;
+			this.pixellist.add(a);
+			paintBall(a);
+		}
+		if (z == null) {
+			z = new int[4];
+			z[0] = (int)(w - w * 0.1 * Math.random());
+			z[1] = (int)(h - h * 0.1 * Math.random());
+			z[2] = (int)(d - d * 0.1 * Math.random());
+			z[3] = 5;
+			this.pixellist.add(z);
+			paintBall(z);
+		}
+
+		System.err.print("(x,y,z)₁ = (" + a[0] + "," + a[1] + "," + a[2] + ") ");
+		System.err.print("(x,y,z)₂ = (" + z[0] + "," + z[1] + "," + z[2] + ")");
+
+		m = new int[4];
+
+		if (Math.abs(a[0] - z[0]) < 2 && Math.abs(a[0] - z[0]) < 2 && Math.abs(a[2] - z[2]) < 2) {
+			System.err.println();
+			return;
+		}
+
+		for (int i = 0; i < 3; i++) {
+			double offset = (double)Math.abs(a[i] - z[i]) * 0.5;
+			m[i] = (int)(((double)(a[i] + z[i]) - offset) / 2.0 + offset * Math.random());
+		}
+		m[3] = (a[3] + z[3]) / 2;
+
+		System.err.println(" (x,y,z)₂ = (" + m[0] + "," + m[1] + "," + m[2] + ")");
+
+		this.pixellist.add(m);
+
+		paintBall(m);
+
+		if ((a[0] & m[0] & ~1) != 0 || (a[1] & m[1] & ~1) != 0 || (a[2] & m[2] & ~1) != 0)
+			paintNeuron(a, m);
+		if ((m[0] & z[0] & ~1) != 0 || (m[1] & m[1] & ~1) != 0 || (m[2] & m[2] & ~1) != 0)
+			paintNeuron(m, z);
+
+		return;
+	}
+
+	private void paintBall(int co[])
+	{
+		int x = co[0], y = co[1], z = co[2], r = co[3];
 		/* Could someone tell me how to indent this nicely to < 80
 		 * characters without completely ripping it apart? */
 
