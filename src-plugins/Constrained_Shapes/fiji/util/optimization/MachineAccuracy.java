c7ce4f8063ade9731a61d3f7d889f942f7de4ce9
Johannes Schindelin <johannes.schindelin@gmx.de>
2010-05-25 23:39:53 +0200

    Turn DOS line endings to Unix ones
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/Constrained_Shapes/fiji/util/optimization/MachineAccuracy.java
+++ src-plugins/Constrained_Shapes/fiji/util/optimization/MachineAccuracy.java
@@ -25,7 +25,7 @@ public class MachineAccuracy
 
 	/** machine accuracy constant */
 	public static double EPSILON = 2.220446049250313E-16;
-	
+
 	public static double SQRT_EPSILON = 1.4901161193847656E-8;
 	public static double SQRT_SQRT_EPSILON = 1.220703125E-4;
 
@@ -39,7 +39,7 @@ public class MachineAccuracy
 			eps /= 2.0;
 		}
 		eps *= 2.0;
-		
+
 		return eps;
 	}
 
