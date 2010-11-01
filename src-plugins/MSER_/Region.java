9b290c946e2f6995f0f92c3d294b5c656200b735
Jan Funke <jan.funke@inf.tu-dresden.de>
2010-11-01 15:39:30 +0100

    MSER_: added ids to regions

--- src-plugins/MSER_/Region.java
+++ src-plugins/MSER_/Region.java
@@ -3,6 +3,10 @@ import java.util.Vector;
 
 public class Region {
 
+	private static int NextId = 0;
+
+	private int id;
+
 	private Region         parent;
 	private Vector<Region> children;
 
@@ -10,6 +14,9 @@ public class Region {
 	private double[] center;
 
 	public Region(int size, double[] center) {
+
+		this.id       = NextId;
+		NextId++;
 
 		this.size     = size;
 		this.center   = new double[center.length];
@@ -23,34 +30,64 @@ public class Region {
 		this.parent = parent;
 	}
 
-	public Vector<Region> getChildren()
-	{
+	public Vector<Region> getChildren() {
 		return this.children;
 	}
 
-	public int getSize()
-	{
+	public int getSize() {
 		return this.size;
 	}
 
-	public double[] getCenter()
-	{
+	public double[] getCenter() {
 		return this.center;
 	}
 
-	public double getCenter(int index)
-	{
+	public double getCenter(int index) {
 		return this.center[index];
 	}
 
+<<<<<<< Updated upstream
 	public Region getParent()
 	{
+=======
+	public void setClosestRegion(Region closestRegion) {
+		this.closestRegion = closestRegion;
+	}
+
+	public void setMinNegLogPAssignment(double minNegLogPAssignment) {
+		this.minNegLogPAssignment = minNegLogPAssignment;
+	}
+
+	public double getMinNegLogPAssignment() {
+		return this.minNegLogPAssignment;
+	}
+
+	public Region getClosestRegion() {
+		return this.closestRegion;
+	}
+
+	public Region getParent() {
+>>>>>>> Stashed changes
 		return this.parent;
 	}
 
 	public void addChildren(Vector<Region> children) {
 
 		this.children.addAll(children);
+	}
+
+	public boolean isAncestorOf(Region other) {
+
+		while (other.getParent() != null)
+			if (other.getParent() == this)
+				return true;
+			else
+				other = other.getParent();
+		return false;
+	}
+
+	public int getId() {
+		return id;
 	}
 
 	public String toString() {
