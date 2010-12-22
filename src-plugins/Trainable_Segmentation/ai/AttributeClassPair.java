f9066b330632b7ab3266cf302b1a477946d24b88
Albert Cardona <acardona@ini.phys.ethz.ch>
2010-12-22 15:16:21 -0500

    TS: AttributeClassPair with final fields

--- src-plugins/Trainable_Segmentation/ai/AttributeClassPair.java
+++ src-plugins/Trainable_Segmentation/ai/AttributeClassPair.java
@@ -4,19 +4,19 @@ package ai;
  * Inner class to order attributes while preserving class indices 
  *
  */
-public class AttributeClassPair
+public final class AttributeClassPair
 {
 	/** real value of the corresponding attribute */
-	double attributeValue;
+	final double attributeValue;
 	/** index of the class associated to this pair */
-	int classValue;
+	final int classValue;
 	/**
 	 * Create pair attribute-class
 	 * 
 	 * @param attributeValue real attribute value
 	 * @param classIndex index of the class associated to this sample
 	 */
-	AttributeClassPair(double attributeValue, int classIndex)
+	AttributeClassPair(final double attributeValue, final int classIndex)
 	{
 		this.attributeValue = attributeValue;
 		this.classValue = classIndex;
