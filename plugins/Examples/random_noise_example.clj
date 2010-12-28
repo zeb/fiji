056fda60a0608a9033dbfab573e40f05fb818241
Robert Snapp <Robert.Snapp@uvm.edu>
2010-12-27 23:12:30 -0500

    Removed the argument '(java.util Random) from the import function call, as this creates a namespace conflict; and subsequently prefixed the subsequent call to Random with the namespace java.util.
    Also introduced the function int-to-ubyte, for converting integers into pseudo-unsigned bytes.
    The functions rand-byte and make-channel were modified to employ int-to-byte, thus fixing an
    exception that was thrown during the creation of the second test image, "random 2" because
    the byte value was out of range.

--- plugins/Examples/random_noise_example.clj
+++ plugins/Examples/random_noise_example.clj
@@ -3,13 +3,22 @@
 (import '(ij.process ByteProcessor)
 	'(ij ImagePlus)
 	'(java.awt.image IndexColorModel)
-	'(java.util Random))
+	)
+
+
+(defn int-to-ubyte
+  "Converts the integer x (arg1) in the range [0, 255] to a byte. Note that bytes in clojure and java are
+regrettably always signed."
+  [ x ]
+  {:pre [(<= 0 x 255)]}
+  (byte (if (< x 128) x (- x 256))))
+
 
 ; A closure over a new instance of Random,
 ; used in an inner function that generates random bytes
-(let [rand (new Random)]
+(let [rand (new java.util.Random)]
   (defn rand-byte []
-	(byte (- (.nextInt rand 256) 128))))
+	(int-to-ubyte (.nextInt rand 256))))
 
 ; Create a new image and set each pixel to a random byte
 (let [bp (new ByteProcessor 512 512)
@@ -23,7 +32,7 @@
       (defn make-channel []
 	    (let [channel (make-array Byte/TYPE 256)]
 	      (dotimes [i 256]
-		(aset channel i (byte i)))
+			(aset channel i (int-to-ubyte i)))
 	      channel))
       (IndexColorModel. 8 256 (make-channel) (make-channel) (make-channel)))
 
