241038ccb28a931f2e64148deb471d80f867fa9a
Johannes Schindelin <johannes.schindelin@gmx.de>
2011-05-06 19:25:07 +0200

    call fiji: support fiji_path() on Windows, too
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/callfiji/CallFiji.c
+++ src-plugins/callfiji/CallFiji.c
@@ -5,33 +5,54 @@
 #ifndef WIN32
 #define __USE_GNU
 #include <dlfcn.h>
+#else
+#include <windows.h>
+
+static char module_path[PATH_MAX];
+
+BOOL WINAPI DllMain(HINSTANCE dll, DWORD reason, LPVOID reserved)
+{
+	if (reason != DLL_PROCESS_ATTACH)
+		return TRUE;
+	GetModuleFileNameA(dll, module_path, sizeof(module_path));
+	return TRUE;
+}
+
 #endif
 
-const char *fiji_path(void)
+JNIEXPORT const char *fiji_path(void)
 {
-#ifdef WIN32
-#else
-	static char path[PATH_MAX];
-	Dl_info info;
+	static char buffer[PATH_MAX];
+	const char *path;
 	int i, end;
+	char slash = '/';
+#ifndef WIN32
+	Dl_info info;
+#endif
 
 	/* static char arrays are initialized to NUL */
-	if (path[0])
-		return path;
+	if (buffer[0])
+		return buffer;
 
+#ifdef WIN32
+	slash = '\\';
+	path = module_path;
+#else
 	if (!dladdr(fiji_path, &info))
 		return NULL;
 
-	/* strip lib/<platform>/<libname> */
-	end = strlen(info.dli_fname);
-	for (i = 0; i < 3; i++)
-		while (--end >= 0 && info.dli_fname[end] != '/')
-			; /* ignore */
-	if (!end || end > sizeof(path))
-		return NULL;
-	strncpy(path, info.dli_fname, end);
-	return path;
+	path = info.dli_fname;
 #endif
+
+	/* strip lib/<platform>/<libname> */
+	end = strlen(path);
+	for (i = 0; i < 3; i++)
+		while (--end >= 0 && path[end] != slash)
+			; /* ignore */
+	if (!end || end > sizeof(buffer))
+		return NULL;
+	strncpy(buffer, path, end);
+	return buffer;
 }
 
 JNIEXPORT void JNICALL Java_CallFiji_run(JNIEnv *env, jclass clazz)
