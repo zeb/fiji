fe06296b7e3968fe6383df000d2c81f82ea06fb8
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2011-05-06 15:53:19 +0200

    FlowMate_: retouch SimoncelliDerivation
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src-plugins/FlowMate_/fiji/plugin/flowmate/SimoncelliDerivation.java
+++ src-plugins/FlowMate_/fiji/plugin/flowmate/SimoncelliDerivation.java
@@ -20,29 +20,102 @@ import mpicbg.imglib.outofbounds.OutOfBoundsStrategyMirrorFactory;
 import mpicbg.imglib.type.numeric.RealType;
 import mpicbg.imglib.type.numeric.real.FloatType;
 
-public class SimoncelliDerivation  < A extends RealType<A>> implements OutputAlgorithm<FloatType>, MultiThreaded, Benchmark {
+/**
+ * Implement Simoncelli matched derivative filters from \cite{simoncelli94}.
+ * <p>
+ * Discrete derivation can be seen as a convolution with a filter, the <i>derivative filter</i> which is the derivation
+ * of some smoothing filter, used for the interpolation of the signal, called here the <i>prefilter</i>.  
+ * Simoncelli's point when designing multi-dimensional filter is that there is a wide range of
+ * situation when the derivative of a signal will be used along the signal itself or with another
+ * derivative. In these case, it make sense to smooth the signal using the prefilter mentioned above, so 
+ * that it matches the amount of filtering done for the calculation of the derivative. 
+ * <p>
+ * Simoncelli proposes to design both filter simultaneously, under the following constraints:
+ * <ul>
+ * 	<li>The derivative filter must be a good approximation to the exact derivation of the prefiltered
+ * signal.
+ * 	<li>The prefilter should have linear, ideally 0, phase.
+ * 	<li>The filters should be separable.
+ * </ul>
+ * <p>
+ * This yields the derivation algorithm implemented here. The image is derived in one dimension using the derivative
+ * filter of Simoncelli, and all other dimensions are filtered using the matching prefilter. We proposed the four 
+ * kernels calculated by Simoncelli, whose support ranges from 2 to 5.
+ * 
+ * <p>
+ * BibTeX:
+ * <pre>
+ * &#64;inproceedings{simoncelli94
+ * 	author = {Eero P. Simoncelli},
+ * 	title = {Design of Multi-Dimensional Derivative Filters},
+ * 	booktitle = {International Conference on Image Processing},
+ * 	year = {1994},
+ * 	pages = {790--794},
+ * 	masid = {327150}
+ * }
+ * </pre>
+ *  
+ * @author Jean-Yves Tinevez
+ * @param <A> the type of the source image. Must a {@link RealType}, for we perform all operations on floats, and
+ * must be able to convert to {@link FloatType}. 
+ */
+public class SimoncelliDerivation<A extends RealType<A>> implements OutputAlgorithm<FloatType>, MultiThreaded, Benchmark {
 
 	private static final String BASE_ERROR_MESSAGE = "SimoncelliDerivation: ";
 
-	private static final double[] SMOOTHING_KERNEL  = new double[] { 
-		0.035697564482688904, 
-		0.24887460470199585,
-		0.4308556616306305, 
-		0.24887460470199585, 
-		0.035697564482688904
+	private static final double[][] SMOOTHING_KERNELS  = new double[][] {
+		{	// Support = 2: central difference
+			0.5,
+			0.5
+		},
+		{ // Support = 3
+			0.22420981526374817,
+			0.5515803694725037,
+			0.22420981526374817
+		},
+		{ // Support = 4
+			0.09156766533851624,
+			0.40843233466148377,
+			0.40843233466148377,
+			0.09156766533851624
+		},
+		{  // Support = 5
+			0.035697564482688904, 
+			0.24887460470199585,
+			0.4308556616306305, 
+			0.24887460470199585, 
+			0.035697564482688904 
+		}
 	}; 
 	
-	private static final double[] DIFFERENTIATING_KERNEL = new double[] {
-		-0.1076628714799881,
-		-0.2826710343360901,
-		0,
-		0.2826710343360901,
-		0.1076628714799881		
+	private static final double[][] DIFFERENTIATING_KERNELS = new double[][] {
+		{ // Support = 2
+			-0.6860408186912537,
+			0.6860408186912537,
+		},
+		{ // Support = 3
+			-0.45527133345603943,
+			0,
+			0.45527133345603943
+		},
+		{ // Support = 4
+			-0.2362217784125595,
+			-0.27143725752830506,
+			0.27143725752830506,
+			0.2362217784125595
+		},
+		{ // Support = 5
+			-0.1076628714799881,
+			-0.2826710343360901,
+			0,
+			0.2826710343360901,
+			0.1076628714799881		
+		}
 	};
 	
-	private static final double[][] KERNELS = new double[][] { // Order is important
-		SMOOTHING_KERNEL,
-		DIFFERENTIATING_KERNEL
+	private static final double[][][] KERNELS = new double[][][] { // Order is important
+		SMOOTHING_KERNELS,
+		DIFFERENTIATING_KERNELS
 	};
 
     private OutOfBoundsStrategyFactory<FloatType> outOfBoundsFactory = new OutOfBoundsStrategyMirrorFactory<FloatType>();
@@ -55,16 +128,24 @@ public class SimoncelliDerivation  < A extends RealType<A>> implements OutputAlg
 
 	private int targetDim;
 
-	private Image<FloatType> tmp;
+	/**
+	 * The kernel size we will use. In this class, we have the kernel values only for support which size is ranging
+	 * from 2 to 5. 
+	 */
+	private int support;
+	/** Temporary holder needed to share data between threads. */
+	private Image<FloatType> source;
 
 
 	
-	public SimoncelliDerivation(Image<A> image) {
+	public SimoncelliDerivation(Image<A> image, final int kernelSize) {
 		this.image = image;
+		this.support = kernelSize;
 		ImageConverter<A, FloatType> converter = new ImageConverter<A, FloatType>(
 				image, 
 				new ImageFactory<FloatType>(new FloatType(), new ImagePlusContainerFactory()), 
 				new RealTypeConverter<A, FloatType>());
+		converter.setNumThreads();
 		converter.checkInput();
 		converter.process();
 		this.floatImage = converter.getResult();
@@ -93,6 +174,10 @@ public class SimoncelliDerivation  < A extends RealType<A>> implements OutputAlg
 			errorMessage = BASE_ERROR_MESSAGE + "Target derivation dimension is incorrect.";
 			return false;
 		}
+		if (support < 2 || support > 5) {
+			errorMessage = BASE_ERROR_MESSAGE + "Only kernel size ranging from 2 to 5 is supported, for "+support+".";
+			return false;
+		}
 		return true;
 	}
 
@@ -101,8 +186,7 @@ public class SimoncelliDerivation  < A extends RealType<A>> implements OutputAlg
 
 		final long startTime = System.currentTimeMillis();
 		final long imageSize = floatImage.getNumPixels();
-		convolved = floatImage.createNewImage();
-		tmp = floatImage.clone();
+		convolved = floatImage.clone();
 		
 		final int[] sequence = new int[floatImage.getNumDimensions()];
 		sequence[targetDim] = 1; // will point to derivation kernel; 0s point to smoothing kernel
@@ -111,8 +195,9 @@ public class SimoncelliDerivation  < A extends RealType<A>> implements OutputAlg
         final Vector<Chunk> threadChunks = SimpleMultiThreading.divideIntoChunks( imageSize, numThreads );
         
         for ( int dim = 0; dim < floatImage.getNumDimensions(); dim++ ) {
+        	source = convolved.clone(); // Source for next iteration
         	final int currentDim = dim;
-        	final double[] kernel = KERNELS[sequence[currentDim]];
+        	final double[] kernel = KERNELS[sequence[currentDim]][support-2];
         	
         	final AtomicInteger ai = new AtomicInteger(0);					
         	final Thread[] threads = SimpleMultiThreading.newThreads( numThreads );
@@ -129,7 +214,7 @@ public class SimoncelliDerivation  < A extends RealType<A>> implements OutputAlg
         				// Get chunk of pixels to process
         				final Chunk myChunk = threadChunks.get( myNumber );
 
-        				final LocalizableByDimCursor<FloatType> inputIterator = tmp.createLocalizableByDimCursor(outOfBoundsFactory);
+        				final LocalizableByDimCursor<FloatType> inputIterator = source.createLocalizableByDimCursor(outOfBoundsFactory);
         				final LocalizableCursor<FloatType> outputIterator = convolved.createLocalizableCursor();
 
         				// Convolve the image in the current dimension using the given cursors
@@ -142,9 +227,9 @@ public class SimoncelliDerivation  < A extends RealType<A>> implements OutputAlg
         		});
 
         	SimpleMultiThreading.startAndJoin(threads);
-        	tmp = convolved.clone(); // Source for next iteration
         	
         }
+        source = null;
 
         String name = image.getName()+"_";
         for (int i = 0; i < sequence.length; i++) {
@@ -155,7 +240,6 @@ public class SimoncelliDerivation  < A extends RealType<A>> implements OutputAlg
 		}
         convolved.setName(name);
         processingTime += System.currentTimeMillis() - startTime;
-
         return true;        
 	}
 
