434fec3eef27c472b904560ef071c725592d0d35
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2011-09-26 22:02:58 +0200

    CWNT_: lay out GUI
    Signed-off-by: Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>

--- src-plugins/CWNT_/fiji/plugin/nucleitracker/gui/CwntGui.java
+++ src-plugins/CWNT_/fiji/plugin/nucleitracker/gui/CwntGui.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
@@ -21,15 +22,16 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 import fiji.plugin.nucleitracker.NucleiMasker;
+import javax.swing.JButton;
 
 public class CwntGui extends JPanel {
 
 	private static final long serialVersionUID = -1739705351534814574L;
-	
+
 	/*
 	 * EVENTS
 	 */
-	
+
 	/** This event is fired whenever the value of σ for the gaussian filtering in step 1 is changed by this GUI. */
 	public final ActionEvent STEP1_PARAMETER_CHANGED = new ActionEvent(this, 0, "GaussianFilteringParameterChanged");
 	/** This event is fired whenever parameters defining the anisotropic diffusion step 2 is changed by this GUI. */
@@ -38,29 +40,31 @@ public class CwntGui extends JPanel {
 	public final ActionEvent STEP3_PARAMETER_CHANGED = new ActionEvent(this, 2, "DerivativesParameterChanged");
 	/** This event is fired whenever parameters specifying how to combine the final mask in step 4 is changed by this GUI. */
 	public final ActionEvent STEP4_PARAMETER_CHANGED = new ActionEvent(this, 3, "MaskingParameterChanged");
-	
+	/** This event is fired when the suer presses the 'go' button in the 4th tab. */
+	public final ActionEvent GO_BUTTON_PRESSED = new ActionEvent(this, 4, "GoButtonPressed");
+
 	/*
 	 * LOCAL LISTENERS
 	 */
-	
+
 	private ArrayList<ActionListener> listeners = new ArrayList<ActionListener>();
-	
+
 	private ChangeListener step1ChangeListener = new ChangeListener() {
 		public void stateChanged(ChangeEvent e) { fireEvent(STEP1_PARAMETER_CHANGED);	}
 	};
-	
+
 	private ChangeListener step2ChangeListener = new ChangeListener() {
 		public void stateChanged(ChangeEvent e) { fireEvent(STEP2_PARAMETER_CHANGED); }
 	};
-	
+
 	private ChangeListener step3ChangeListener = new ChangeListener() {
 		public void stateChanged(ChangeEvent e) { fireEvent(STEP3_PARAMETER_CHANGED); }
 	};
-	
+
 	private ChangeListener step4ChangeListener = new ChangeListener() {
 		public void stateChanged(ChangeEvent e) { fireEvent(STEP4_PARAMETER_CHANGED); }
 	};
-	
+
 	private KeyListener step1KeyListener = new KeyAdapter() {
 		public void keyReleased(KeyEvent ke) { fireEvent(STEP1_PARAMETER_CHANGED); 	}
 	};
@@ -77,12 +81,12 @@ public class CwntGui extends JPanel {
 		public void keyReleased(KeyEvent ke) { fireEvent(STEP4_PARAMETER_CHANGED); 	}
 	};
 
-	
-	
+
+
 	/*
 	 * FONTS
 	 */
-	
+
 	private final static Font SMALL_LABEL_FONT = new Font("Arial", Font.PLAIN, 12);
 	private final static Font MEDIUM_LABEL_FONT = new Font("Arial", Font.PLAIN, 16);
 	private final static Font BIG_LABEL_FONT = new Font("Arial", Font.PLAIN, 20);
@@ -91,7 +95,7 @@ public class CwntGui extends JPanel {
 	/*
 	 * PARAMETERS
 	 */
-	
+
 	private double[] oldParams;
 
 	/*
@@ -136,32 +140,39 @@ public class CwntGui extends JPanel {
 
 	private JTabbedPane tabbedPane;
 
+	private String targetImageName;
+
 	/*
 	 * CONSTRUCTORS
 	 */
-	
-	public CwntGui() {
+
+	/**
+	 * @wbp.parser.constructor
+	 */
+	public CwntGui(String targetImageName) {
+		this.targetImageName = targetImageName;
 		initGUI();
 	}
 
-	public CwntGui(double[] params) {
+	public CwntGui(String targetImageName, double[] params) {
+		this.targetImageName = targetImageName;
 		setParameters(params);
 		initGUI();
 	}
 
-	
+
 	/*
 	 * PUBLIC METHODS
 	 */
-	
+
 	public boolean addActionListener(ActionListener listener) {
 		return listeners.add(listener);
 	}
-	
+
 	public boolean removeActionListener(ActionListener listener) {
 		return listeners.remove(listener);
 	}
-	
+
 	public List<ActionListener> getActionListeners() {
 		return listeners;
 	}
@@ -185,19 +196,19 @@ public class CwntGui extends JPanel {
 	public double[] getParameters() {
 		return params;
 	}
-	
+
 	public void setParameters(double[] params) {
 		System.arraycopy(params, 0, this.params, 0, params.length);
 	}
-	
+
 	public int getSelectedIndex() {
 		return tabbedPane.getSelectedIndex();
 	}
-	
+
 	/*
 	 * PRIVATE METHODS
 	 */
-	
+
 	private double[] collectParameters() throws NumberFormatException {
 		double gaussFilterSigma = Double.parseDouble(gaussFiltSigmaText.getText());
 		double nIterAnDiff = (int)  Double.parseDouble(aniDiffNIterText.getText());
@@ -220,7 +231,7 @@ public class CwntGui extends JPanel {
 				delta
 		};
 	}
-	
+
 	private void fireEvent(final ActionEvent event) {
 		try {
 			params = collectParameters();
@@ -235,44 +246,71 @@ public class CwntGui extends JPanel {
 			listener.actionPerformed(event);
 		}
 	}
-	
+
 	private void initGUI() {
 		setLayout(new BorderLayout());
 
 		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		add(tabbedPane);
+		{
+			JPanel panelIntroduction = new JPanel();
+			tabbedPane.addTab("Intro", null, panelIntroduction, null);
+			panelIntroduction.setLayout(null);
+
+			JLabel lblCrownwearingNucleiTracker = new JLabel("Crown-Wearing Nuclei Tracker");
+			lblCrownwearingNucleiTracker.setFont(BIG_LABEL_FONT);
+			lblCrownwearingNucleiTracker.setHorizontalAlignment(SwingConstants.CENTER);
+			lblCrownwearingNucleiTracker.setBounds(10, 11, 325, 30);
+			panelIntroduction.add(lblCrownwearingNucleiTracker);
+
+			JLabel lblTargetImage = new JLabel("Target image:");
+			lblTargetImage.setFont(MEDIUM_LABEL_FONT);
+			lblTargetImage.setBounds(10, 52, 325, 19);
+			panelIntroduction.add(lblTargetImage);
+
+			JLabel labelTargetImage = new JLabel(targetImageName);
+			labelTargetImage.setHorizontalAlignment(SwingConstants.CENTER);
+			labelTargetImage.setFont(MEDIUM_LABEL_FONT);
+			labelTargetImage.setBounds(10, 82, 325, 19);
+			panelIntroduction.add(labelTargetImage);
+
+			JLabel labelIntro = new JLabel(INTRO_TEXT);
+			labelIntro.setFont(SMALL_LABEL_FONT.deriveFont(11f));
+			labelIntro.setBounds(10, 112, 325, 358);
+			panelIntroduction.add(labelIntro);
+		}
 
 		{
-			JPanel panel1 = new JPanel();
-			tabbedPane.addTab("Parameter set 1", null, panel1, null);
-			panel1.setLayout(null);
+			JPanel panelParams1 = new JPanel();
+			tabbedPane.addTab("Param set 1", null, panelParams1, null);
+			panelParams1.setLayout(null);
 
 			JLabel lblNewLabel = new JLabel("Parameter set 1");
 			lblNewLabel.setFont(BIG_LABEL_FONT);
 			lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 			lblNewLabel.setBounds(10, 11, 325, 29);
-			panel1.add(lblNewLabel);
+			panelParams1.add(lblNewLabel);
 
 			JLabel lblFiltering = new JLabel("1. Filtering");
 			lblFiltering.setFont(MEDIUM_LABEL_FONT);
 			lblFiltering.setBounds(10, 64, 325, 29);
-			panel1.add(lblFiltering);
+			panelParams1.add(lblFiltering);
 
 			{
 				JLabel lblGaussianFilter = new JLabel("Gaussian filter \u03C3:");
 				lblGaussianFilter.setFont(SMALL_LABEL_FONT);
 				lblGaussianFilter.setBounds(10, 104, 325, 14);
-				panel1.add(lblGaussianFilter);
+				panelParams1.add(lblGaussianFilter);
 
 				gaussFiltSigmaSlider = new DoubleJSlider(0, 5*scale, (int) (params[0]*scale), scale);
 				gaussFiltSigmaSlider.setBounds(10, 129, 255, 23);
-				panel1.add(gaussFiltSigmaSlider);
+				panelParams1.add(gaussFiltSigmaSlider);
 
 				gaussFiltSigmaText = new JTextField(""+params[0]);
 				gaussFiltSigmaText.setHorizontalAlignment(SwingConstants.CENTER);
 				gaussFiltSigmaText.setBounds(275, 129, 60, 23);
 				gaussFiltSigmaText.setFont(TEXT_FIELD_FONT);
-				panel1.add(gaussFiltSigmaText);
+				panelParams1.add(gaussFiltSigmaText);
 
 				link(gaussFiltSigmaSlider, gaussFiltSigmaText);
 				gaussFiltSigmaSlider.addChangeListener(step1ChangeListener);
@@ -282,24 +320,24 @@ public class CwntGui extends JPanel {
 			JLabel lblAnisotropicDiffusion = new JLabel("2. Anisotropic diffusion");
 			lblAnisotropicDiffusion.setFont(MEDIUM_LABEL_FONT);
 			lblAnisotropicDiffusion.setBounds(10, 181, 325, 29);
-			panel1.add(lblAnisotropicDiffusion);
+			panelParams1.add(lblAnisotropicDiffusion);
 
 			{
 				JLabel lblNumberOfIterations = new JLabel("Number of iterations:");
 				lblNumberOfIterations.setFont(SMALL_LABEL_FONT);
 				lblNumberOfIterations.setBounds(10, 221, 325, 14);
-				panel1.add(lblNumberOfIterations);
+				panelParams1.add(lblNumberOfIterations);
 
 				aniDiffNIterText = new JTextField();
 				aniDiffNIterText.setHorizontalAlignment(SwingConstants.CENTER);
 				aniDiffNIterText.setText(""+params[1]);
 				aniDiffNIterText.setFont(TEXT_FIELD_FONT);
 				aniDiffNIterText.setBounds(275, 246, 60, 23);
-				panel1.add(aniDiffNIterText);
+				panelParams1.add(aniDiffNIterText);
 
 				aniDiffNIterSlider = new DoubleJSlider(1, 10, (int) params[1], 1);
 				aniDiffNIterSlider.setBounds(10, 246, 255, 23);
-				panel1.add(aniDiffNIterSlider);
+				panelParams1.add(aniDiffNIterSlider);
 
 				link(aniDiffNIterSlider, aniDiffNIterText);
 				aniDiffNIterSlider.addChangeListener(step2ChangeListener);
@@ -311,18 +349,18 @@ public class CwntGui extends JPanel {
 				JLabel lblGradientDiffusionThreshold = new JLabel("Gradient diffusion threshold \u03BA:");
 				lblGradientDiffusionThreshold.setFont(SMALL_LABEL_FONT);
 				lblGradientDiffusionThreshold.setBounds(10, 280, 325, 14);
-				panel1.add(lblGradientDiffusionThreshold);
+				panelParams1.add(lblGradientDiffusionThreshold);
 
 				aniDiffKappaText = new JTextField();
 				aniDiffKappaText.setHorizontalAlignment(SwingConstants.CENTER);
 				aniDiffKappaText.setText(""+params[2]);
 				aniDiffKappaText.setFont(TEXT_FIELD_FONT);
 				aniDiffKappaText.setBounds(275, 305, 60, 23);
-				panel1.add(aniDiffKappaText);
+				panelParams1.add(aniDiffKappaText);
 
 				aniDiffKappaSlider = new DoubleJSlider(1, 100, (int) params[2], 1);
 				aniDiffKappaSlider.setBounds(10, 305, 255, 23);
-				panel1.add(aniDiffKappaSlider);
+				panelParams1.add(aniDiffKappaSlider);
 
 				link(aniDiffKappaSlider, aniDiffKappaText);
 				aniDiffKappaSlider.addChangeListener(step2ChangeListener);
@@ -332,63 +370,63 @@ public class CwntGui extends JPanel {
 			JLabel lblDerivativesCalculation = new JLabel("3. Derivatives calculation");
 			lblDerivativesCalculation.setFont(new Font("Arial", Font.PLAIN, 16));
 			lblDerivativesCalculation.setBounds(10, 351, 325, 29);
-			panel1.add(lblDerivativesCalculation);
+			panelParams1.add(lblDerivativesCalculation);
 
 			{
 				JLabel lblGaussianGradient = new JLabel("Gaussian gradient \u03C3:");
 				lblGaussianGradient.setFont(new Font("Arial", Font.PLAIN, 12));
 				lblGaussianGradient.setBounds(10, 391, 325, 14);
-				panel1.add(lblGaussianGradient);
+				panelParams1.add(lblGaussianGradient);
 
 				gaussGradSigmaText = new JTextField();
 				gaussGradSigmaText.setHorizontalAlignment(SwingConstants.CENTER);
 				gaussGradSigmaText.setText(""+params[3]);
 				gaussGradSigmaText.setBounds(275, 416, 60, 23);
-				panel1.add(gaussGradSigmaText);
+				panelParams1.add(gaussGradSigmaText);
 
 				gaussGradSigmaSlider = new DoubleJSlider(0, 5*scale, (int) (params[3]*scale), scale);
 				gaussGradSigmaSlider.setBounds(10, 416, 255, 23);
-				panel1.add(gaussGradSigmaSlider);
+				panelParams1.add(gaussGradSigmaSlider);
 
 				link(gaussGradSigmaSlider, gaussGradSigmaText);
 				gaussGradSigmaSlider.addChangeListener(step3ChangeListener);
 				gaussGradSigmaText.addKeyListener(step3KeyListener);
 			}
-			
+
 		}
 
 		{
-			JPanel panel2 = new JPanel();
-			tabbedPane.addTab("Parameter set 2", null, panel2, null);
-			panel2.setLayout(null);
+			JPanel panelParams2 = new JPanel();
+			tabbedPane.addTab("Param set 2", null, panelParams2, null);
+			panelParams2.setLayout(null);
 
 			JLabel lblParameterSet = new JLabel("Parameter set 2");
 			lblParameterSet.setHorizontalAlignment(SwingConstants.CENTER);
 			lblParameterSet.setFont(BIG_LABEL_FONT);
 			lblParameterSet.setBounds(10, 11, 325, 29);
-			panel2.add(lblParameterSet);
+			panelParams2.add(lblParameterSet);
 
 			JLabel lblMasking = new JLabel("4. Masking");
 			lblMasking.setFont(MEDIUM_LABEL_FONT);
 			lblMasking.setBounds(10, 51, 325, 29);
-			panel2.add(lblMasking);
+			panelParams2.add(lblMasking);
 
 
 			{
 				JLabel gammeLabel = new JLabel("\u03B3: tanh shift");
 				gammeLabel.setFont(SMALL_LABEL_FONT);
 				gammeLabel.setBounds(10, 106, 325, 14);
-				panel2.add(gammeLabel);
+				panelParams2.add(gammeLabel);
 
 				gammaSlider = new DoubleJSlider(-5*scale, 5*scale, (int) (params[4]*scale), scale);
 				gammaSlider.setBounds(10, 131, 255, 23);
-				panel2.add(gammaSlider);
+				panelParams2.add(gammaSlider);
 
 				gammaText = new JTextField();
 				gammaText.setText(""+params[4]);
 				gammaText.setFont(TEXT_FIELD_FONT);
 				gammaText.setBounds(275, 131, 60, 23);
-				panel2.add(gammaText);
+				panelParams2.add(gammaText);
 
 				link(gammaSlider, gammaText);
 				gammaSlider.addChangeListener(step4ChangeListener);
@@ -398,16 +436,16 @@ public class CwntGui extends JPanel {
 				JLabel lblNewLabel_3 = new JLabel("\u03B1: gradient prefactor");
 				lblNewLabel_3.setFont(SMALL_LABEL_FONT);
 				lblNewLabel_3.setBounds(10, 165, 325, 14);
-				panel2.add(lblNewLabel_3);
+				panelParams2.add(lblNewLabel_3);
 
 				alphaSlider = new DoubleJSlider(0, 20*scale, (int) (params[5]*scale), scale);
 				alphaSlider.setBounds(10, 190, 255, 23);
-				panel2.add(alphaSlider);
+				panelParams2.add(alphaSlider);
 
 				alphaText = new JTextField(""+params[5]);
 				alphaText.setFont(TEXT_FIELD_FONT);
 				alphaText.setBounds(275, 190, 60, 23);
-				panel2.add(alphaText);
+				panelParams2.add(alphaText);
 
 				link(alphaSlider, alphaText);
 				alphaSlider.addChangeListener(step4ChangeListener);
@@ -417,17 +455,17 @@ public class CwntGui extends JPanel {
 				JLabel betaLabel = new JLabel("\u03B2: positive laplacian magnitude prefactor");
 				betaLabel.setFont(SMALL_LABEL_FONT);
 				betaLabel.setBounds(10, 224, 325, 14);
-				panel2.add(betaLabel);
+				panelParams2.add(betaLabel);
 
 				betaSlider = new DoubleJSlider(0, 20*scale, (int) (params[6]*scale), scale);
 				betaSlider.setBounds(10, 249, 255, 23);
-				panel2.add(betaSlider);
+				panelParams2.add(betaSlider);
 
 				betaText = new JTextField();
 				betaText.setFont(TEXT_FIELD_FONT);
 				betaText.setText(""+params[6]);
 				betaText.setBounds(275, 249, 60, 23);
-				panel2.add(betaText);
+				panelParams2.add(betaText);
 
 				link(betaSlider, betaText);
 				betaSlider.addChangeListener(step4ChangeListener);
@@ -437,17 +475,17 @@ public class CwntGui extends JPanel {
 				JLabel epsilonLabel = new JLabel("\u03B5: negative hessian magnitude");
 				epsilonLabel.setFont(SMALL_LABEL_FONT);
 				epsilonLabel.setBounds(10, 283, 325, 14);
-				panel2.add(epsilonLabel);
+				panelParams2.add(epsilonLabel);
 
 				epsilonSlider = new DoubleJSlider(0, 20*scale, (int) (scale*params[7]), scale);
 				epsilonSlider.setBounds(10, 308, 255, 23);
-				panel2.add(epsilonSlider);
+				panelParams2.add(epsilonSlider);
 
 				epsilonText = new JTextField();
 				epsilonText.setFont(TEXT_FIELD_FONT);
 				epsilonText.setText(""+params[7]);
 				epsilonText.setBounds(275, 308, 60, 23);
-				panel2.add(epsilonText);
+				panelParams2.add(epsilonText);
 
 				link(epsilonSlider, epsilonText);
 				epsilonSlider.addChangeListener(step4ChangeListener);
@@ -457,17 +495,17 @@ public class CwntGui extends JPanel {
 				JLabel deltaLabel = new JLabel("\u03B4: derivatives sum scale");
 				deltaLabel.setFont(SMALL_LABEL_FONT);
 				deltaLabel.setBounds(20, 342, 325, 14);
-				panel2.add(deltaLabel);
+				panelParams2.add(deltaLabel);
 
 				deltaText = new JTextField();
 				deltaText.setFont(TEXT_FIELD_FONT);
 				deltaText.setText(""+params[8]);
 				deltaText.setBounds(275, 367, 60, 23);
-				panel2.add(deltaText);
+				panelParams2.add(deltaText);
 
 				deltaSlider = new DoubleJSlider(0, 5*scale, (int) (params[8]*scale), scale);
 				deltaSlider.setBounds(10, 367, 255, 23);
-				panel2.add(deltaSlider);
+				panelParams2.add(deltaSlider);
 
 				link(deltaSlider, deltaText);
 				deltaSlider.addChangeListener(step4ChangeListener);
@@ -478,8 +516,34 @@ public class CwntGui extends JPanel {
 			lblEquation.setHorizontalAlignment(SwingConstants.CENTER);
 			lblEquation.setFont(MEDIUM_LABEL_FONT);
 			lblEquation.setBounds(10, 413, 325, 35);
-			panel2.add(lblEquation);
+			panelParams2.add(lblEquation);
 
+		}
+		
+		{
+			JPanel panelRun = new JPanel();
+			tabbedPane.addTab("Run", null, panelRun, null);
+			panelRun.setLayout(null);
+
+			JLabel lblLaunchComputation = new JLabel("Launch computation");
+			lblLaunchComputation.setFont(BIG_LABEL_FONT);
+			lblLaunchComputation.setHorizontalAlignment(SwingConstants.CENTER);
+			lblLaunchComputation.setBounds(10, 11, 325, 31);
+			panelRun.add(lblLaunchComputation);
+
+			JLabel lblEstimatedTimeFor = new JLabel("Estimated time for completion:");
+			lblEstimatedTimeFor.setFont(SMALL_LABEL_FONT);
+			lblEstimatedTimeFor.setBounds(10, 71, 325, 23);
+			panelRun.add(lblEstimatedTimeFor);
+
+			JButton btnGo = new JButton("Go!");
+			btnGo.setFont(MEDIUM_LABEL_FONT);
+			btnGo.setIcon(new ImageIcon(CwntGui.class.getResource("resources/plugin_go.png")));
+			btnGo.setBounds(120, 135, 100, 50);
+			btnGo.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) { fireEvent(GO_BUTTON_PRESSED);	}
+			});
+			panelRun.add(btnGo);
 		}
 	}
 
@@ -504,7 +568,39 @@ public class CwntGui extends JPanel {
 			}
 		});
 	}
-
-
-
+	
+	
+	private static final String INTRO_TEXT = "<html>" +
+			"<div align=\"justify\">" +
+			"This plugin allows the segmentation and tracking of bright blobs objects, " +
+			"typically nuclei imaged in 3D over time. " +
+			"<p> " +
+			"It is specially designed to deal with the case the developing zebra-fish " +
+			"embryogenesis, where nuclei are densily packed, which complicates their detection. " +
+			"To do so, this plugin operates in 2 steps:" +
+			"<p>" +
+			" - The image is first pre-processed, by computing a special mask that stresses" +
+			"the nuclei boundaries. A crown-like mak is computed from the 2D spatial derivatives " +
+			"of the image, and a masked image where the nuclei are better separated is generated. " +
+			"<br>" +
+			" - Then the nuclei are thresholded from the background of the masked image, " +
+			"labeled in 3D and tracked over time. " +
+			"<p>" +
+			"Because the crown-like mask needs 9 parameters to be specified, this plugin offers " +
+			"to test the value of paramters in the 2nd and 3rd tab of this GUI. The resulting masked" +
+			"image and intermediate images will be computed over a limited area of the source image, " +
+			"specified by the ROI. " +
+			"<p> " +
+			"Once you are happy with the parameters, mode to the 4th tab to launch the computation " +
+			"in batch." +
+			"</div>" +
+			"<div align=\"right\"> " +
+			"<tt>" +
+			"Bhavna Rajasekaran <br>" +
+			"Jean-Yves Tinevez <br>" +
+			"Andrew Oates lab - MPI-CBG, Dresden, 2011 " +
+			"</tt>" +
+			"</div>" +
+			"</html>" +
+			"";
 }
