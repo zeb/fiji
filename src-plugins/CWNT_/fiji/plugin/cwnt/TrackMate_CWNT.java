eb21689cb10e5df652f736fb28afbcdf2efb483b
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2011-10-24 21:47:55 +0200

    CWNT_: add a Fiji entry point for CWNT in TrackMate
    Signed-off-by: Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>

---
+++ src-plugins/CWNT_/fiji/plugin/cwnt/TrackMate_CWNT.java
@@ -0,0 +1,54 @@
+package fiji.plugin.cwnt;
+
+import fiji.plugin.cwnt.segmentation.CrownWearingSegmenter;
+import fiji.plugin.trackmate.TrackMate_;
+import fiji.plugin.trackmate.features.spot.BlobDescriptiveStatistics;
+import fiji.plugin.trackmate.features.spot.SpotFeatureAnalyzer;
+import fiji.plugin.trackmate.segmentation.SpotSegmenter;
+import fiji.plugin.trackmate.visualization.TrackMateModelView;
+import ij.plugin.PlugIn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * An entry for CWNT that uses {@link TrackMate_} as a GUI.
+ * @author Jean-Yves Tinevez 2011
+ *
+ */
+public class TrackMate_CWNT implements PlugIn {
+
+	@Override
+	public void run(String arg0) {
+
+		TrackMate_ plugin = new TrackMate_() {
+
+			@SuppressWarnings("rawtypes")
+			@Override
+			protected List<SpotSegmenter> createSegmenterList() {
+				List<SpotSegmenter> list = super.createSegmenterList();
+				list.add(new CrownWearingSegmenter());
+				return list;
+			}
+
+			@Override
+			protected List<SpotFeatureAnalyzer> createSpotFeatureAnalyzerList() {
+				List<SpotFeatureAnalyzer> analyzers = new ArrayList<SpotFeatureAnalyzer>(1);
+				analyzers.add(new BlobDescriptiveStatistics());
+				return analyzers;
+			}
+
+			@Override
+			protected List<TrackMateModelView> createTrackMateModelViewList() {
+				List<TrackMateModelView> views = new ArrayList<TrackMateModelView>(1);
+				views.add(CWNT_.createLocalSliceDisplayer());
+				return views;
+			}
+
+		};
+
+		plugin.run(arg0);
+
+	}
+
+}
