8f68724419a71b75500ccf78230853ff886225f7
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2012-01-11 22:18:28 +0100

    CWNT_: beta 3
    - init filter removed
    - sub-panels in segmentataion config panel sorted
    - tool tips added
    - clean choices
    - bug fixes
    Signed-off-by: Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>

--- src-plugins/CWNT_/fiji/plugin/cwnt/TrackMate_CWNT.java
+++ src-plugins/CWNT_/fiji/plugin/cwnt/TrackMate_CWNT.java
@@ -8,7 +8,22 @@ import java.util.List;
 import fiji.plugin.cwnt.segmentation.CrownWearingSegmenter;
 import fiji.plugin.trackmate.TrackMate_;
 import fiji.plugin.trackmate.features.spot.SpotFeatureAnalyzer;
+import fiji.plugin.trackmate.gui.ActionChooserPanel;
+import fiji.plugin.trackmate.gui.DisplayerChoiceDescriptor;
+import fiji.plugin.trackmate.gui.DisplayerPanel;
+import fiji.plugin.trackmate.gui.InitFilterPanel;
+import fiji.plugin.trackmate.gui.LaunchDisplayerDescriptor;
+import fiji.plugin.trackmate.gui.LoadDescriptor;
+import fiji.plugin.trackmate.gui.SaveDescriptor;
+import fiji.plugin.trackmate.gui.SegmentationDescriptor;
+import fiji.plugin.trackmate.gui.SegmenterChoiceDescriptor;
+import fiji.plugin.trackmate.gui.SpotFilterDescriptor;
+import fiji.plugin.trackmate.gui.StartDialogPanel;
+import fiji.plugin.trackmate.gui.TrackFilterDescriptor;
+import fiji.plugin.trackmate.gui.TrackerChoiceDescriptor;
+import fiji.plugin.trackmate.gui.TrackingDescriptor;
 import fiji.plugin.trackmate.gui.WizardController;
+import fiji.plugin.trackmate.gui.WizardPanelDescriptor;
 import fiji.plugin.trackmate.segmentation.SpotSegmenter;
 import fiji.plugin.trackmate.tracking.SpotTracker;
 import fiji.plugin.trackmate.tracking.kdtree.NearestNeighborTracker;
@@ -16,7 +31,7 @@ import fiji.plugin.trackmate.visualization.TrackMateModelView;
 
 /**
  * An entry for CWNT that uses {@link TrackMate_} as a GUI.
- * @author Jean-Yves Tinevez 2011
+ * @author Jean-Yves Tinevez 2011-2012
  *
  */
 public class TrackMate_CWNT implements PlugIn {
@@ -46,7 +61,7 @@ public class TrackMate_CWNT implements PlugIn {
 				views.add(CWNT_.createLocalSliceDisplayer());
 				return views;
 			}
-			
+
 			@Override
 			protected List<SpotTracker> createSpotTrackerList() {
 				List<SpotTracker> trackers = new ArrayList<SpotTracker>(1);
@@ -56,15 +71,54 @@ public class TrackMate_CWNT implements PlugIn {
 
 			@Override
 			protected void launchGUI() {
-				WizardController controller = new WizardController(this);
-				controller.getWizard().setTitle("CWNT - β2");
+				WizardController controller = new WizardController(this) {
+
+					@Override
+					protected List<WizardPanelDescriptor> createWizardPanelDescriptorList() {
+						List<WizardPanelDescriptor> descriptors = new ArrayList<WizardPanelDescriptor>(14);
+						descriptors.add(new StartDialogPanel());
+						descriptors.add(new SegmenterChoiceDescriptor());
+						descriptors.add(new CWNTSegmentationDescriptor());
+						descriptors.add(new CWMNTDisplayerChoiceDescriptor());
+						descriptors.add(new LaunchDisplayerDescriptor());
+						descriptors.add(new SpotFilterDescriptor());
+						descriptors.add(new TrackerChoiceDescriptor());
+						descriptors.add(new TrackingDescriptor());
+						descriptors.add(new TrackFilterDescriptor());
+						descriptors.add(new DisplayerPanel());
+						descriptors.add(new ActionChooserPanel(this.plugin));
+						descriptors.add(new InitFilterPanel()); // We put it, even if we skip it, so that we avoid NPE when loading
+
+						descriptors.add(new LoadDescriptor());
+						descriptors.add(new SaveDescriptor());
+						return descriptors;
+					}
+
+				};
+				controller.getWizard().setTitle("CWNT - β3");
 			}
-
-
 		};
 
 		plugin.run(arg0);
 
 	}
+
+	private class  CWMNTDisplayerChoiceDescriptor extends DisplayerChoiceDescriptor {
+		@Override
+		public String getPreviousDescriptorID() {
+			// So as to skip the initfilter step in the GUI
+			return CWNTSegmentationDescriptor.DESCRIPTOR;
+		}
+
+	}
+
+	private class  CWNTSegmentationDescriptor extends SegmentationDescriptor {
+		@Override
+		public String getNextDescriptorID() {
+			// So as to skip the initfilter step in the GUI
+			return CWMNTDisplayerChoiceDescriptor.DESCRIPTOR;
+		}
+	}
+
 
 }
--- src-plugins/CWNT_/fiji/plugin/cwnt/test/Test_Integration.java
+++ src-plugins/CWNT_/fiji/plugin/cwnt/test/Test_Integration.java
@@ -5,46 +5,14 @@ import ij.ImageJ;
 import ij.ImagePlus;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
-import fiji.plugin.cwnt.CWNT_;
-import fiji.plugin.cwnt.segmentation.CrownWearingSegmenter;
-import fiji.plugin.trackmate.TrackMate_;
-import fiji.plugin.trackmate.features.spot.BlobDescriptiveStatistics;
-import fiji.plugin.trackmate.features.spot.SpotFeatureAnalyzer;
-import fiji.plugin.trackmate.segmentation.SpotSegmenter;
-import fiji.plugin.trackmate.visualization.TrackMateModelView;
+import fiji.plugin.cwnt.TrackMate_CWNT;
 
 public class Test_Integration {
 	
 	public static void main(String[] args) {
 		
-		TrackMate_ plugin = new TrackMate_() {
-			
-			@SuppressWarnings("rawtypes")
-			@Override
-			protected List<SpotSegmenter> createSegmenterList() {
-				List<SpotSegmenter> list = super.createSegmenterList();
-				list.add(new CrownWearingSegmenter());
-				return list;
-			}
-			
-			@Override
-			protected List<SpotFeatureAnalyzer> createSpotFeatureAnalyzerList() {
-				List<SpotFeatureAnalyzer> analyzers = new ArrayList<SpotFeatureAnalyzer>(1);
-				analyzers.add(new BlobDescriptiveStatistics());
-				return analyzers;
-			}
-			
-			@Override
-			protected List<TrackMateModelView> createTrackMateModelViewList() {
-				List<TrackMateModelView> views = new ArrayList<TrackMateModelView>(1);
-				views.add(CWNT_.createLocalSliceDisplayer());
-				return views;
-			}
-			
-		};
+		TrackMate_CWNT plugin = new TrackMate_CWNT();
 
 		
 //		File testImage = new File("/Users/tinevez/Projects/BRajasekaran/Data/Meta-nov7mdb18ssplus-embryo2-2.tif");
