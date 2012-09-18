6fa8701a508a4a81d2d09344341bb05fd465b935
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2012-09-18 18:01:32 +0200

    MultiViewTracker_: minor tweaks to the example
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src-plugins/MultiViewTracker_/src/main/java/fiji/plugin/multiviewtracker/MultiViewExample.java
+++ src-plugins/MultiViewTracker_/src/main/java/fiji/plugin/multiviewtracker/MultiViewExample.java
@@ -13,36 +13,46 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import mpicbg.models.AbstractAffineModel3D;
 import mpicbg.models.AffineModel3D;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 
 public class MultiViewExample {
 
-	public static <T extends RealType<T> & NativeType<T>, M extends AbstractAffineModel3D< AffineModel3D >> void main(String[] args) {
+	public static <T extends RealType<T> & NativeType<T>> void main(String[] args) {
 
 		// Launch ImageJ
 		ImageJ.main(args);
-		
+
 		// Open several imp
 		List<ImagePlus> imps = new ArrayList<ImagePlus>();
 
-//		ImagePlus imp1 = IJ.openImage("/Users/tinevez/Desktop/Data/Celegans-XY.tif");
-		ImagePlus imp1 = IJ.openImage("E:/Users/JeanYves/Desktop/Data/Celegans.tif");
+		ImagePlus imp1, imp2, imp3;
+
+//		if (IJ.isWindows()) {
+//			imp1 = IJ.openImage("E:/Users/JeanYves/Desktop/Data/Celegans.tif");
+//			imp2 = IJ.openImage("E:/Users/JeanYves/Desktop/Data/Celegans_XZ.tif");
+//			imp3 = IJ.openImage("E:/Users/JeanYves/Desktop/Data/Celegans_YZ.tif");
+//		} else {
+//			imp1 = IJ.openImage("/Users/tinevez/Desktop/Data/Celegans-XY.tif");
+//			imp2 = IJ.openImage("/Users/tinevez/Desktop/Data/Celegans-XZ.tif");
+//			imp3 = IJ.openImage("/Users/tinevez/Desktop/Data/Celegans-YZ.tif");
+//		}
+
+		imp1 = IJ.openImage("../Celegans-XY.tif");
+		imp2 = IJ.openImage("../Celegans-XZ.tif");
+		imp3 = IJ.openImage("../Celegans-YZ.tif");
+
+		
 		imp1.show();
 		imps.add(imp1);
-		
-//		ImagePlus imp2 = IJ.openImage("/Users/tinevez/Desktop/Data/Celegans-XZ.tif");
-		ImagePlus imp2 = IJ.openImage("E:/Users/JeanYves/Desktop/Data/Celegans_XZ.tif");
+
 		imp2.show();
 		imps.add(imp2);
-		
-//		ImagePlus imp3 = IJ.openImage("/Users/tinevez/Desktop/Data/Celegans-YZ.tif");
-		ImagePlus imp3 = IJ.openImage("E:/Users/JeanYves/Desktop/Data/Celegans_YZ.tif");
+
 		imp3.show();
 		imps.add(imp3);
-		
+
 		// Transforms
 		Map<ImagePlus, AffineModel3D> transforms = new HashMap<ImagePlus, AffineModel3D>();
 
@@ -53,7 +63,7 @@ public class MultiViewExample {
 				0, 			(float) (1/calib1[1]), 	0, 			0, 
 				0, 			0, 			(float) (1/calib1[2]), 0);
 		transforms.put(imp1, identity);
-		
+
 
 		double[] calib2 = TMUtils.getSpatialCalibration(imp2);
 		AffineModel3D projXZ = new AffineModel3D();
@@ -62,7 +72,7 @@ public class MultiViewExample {
 				0, 			0, 		(float) ( - 1 / calib2[1] ), 	imp2.getHeight(), 
 				0, 			(float) ( 1 / calib2[2]), 		0,  		0 );
 		transforms.put(imp2, projXZ);
-		
+
 		AffineModel3D projYZ = new AffineModel3D();
 		double[] calib3 = TMUtils.getSpatialCalibration(imp3);
 		projYZ.set( 
@@ -70,18 +80,18 @@ public class MultiViewExample {
 				0, (float) (1/calib3[1]), 0, 0, 
 				(float) ( - 1 / calib3[2] ), 0, 0, (float) ( imp3.getNSlices() ));
 		transforms.put(imp3, projYZ);
-		
-		
+
+
 		// Instantiate model
 		Settings<T> settings = new Settings<T>(imp1);
 		TrackMate_<T> plugin = new TrackMate_<T>(settings);
 		TrackMateModel<T> model = plugin.getModel();
-		
+
 		// Initialize viewer
 		MultiViewDisplayer<T> viewer = new MultiViewDisplayer<T>(imps, transforms, model);
 		viewer.render();
-		
-		
+
+
 	}
 
 }
