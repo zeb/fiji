d1d459da0c8d2d0220f2ecf6f448d31b366e6b3e
Larry Lindsey <llindsey@clm.utexas.edu>
2013-03-04 18:34:29 -0600

    Fiji Archipelago: Support older ImageJ ImagePlus's, as suggested by Johannes

--- src-plugins/Fiji_Archipelago/src/main/java/edu/utexas/clm/archipelago/ijsupport/FloatProcessorChunk.java
+++ src-plugins/Fiji_Archipelago/src/main/java/edu/utexas/clm/archipelago/ijsupport/FloatProcessorChunk.java
@@ -43,7 +43,7 @@ public class FloatProcessorChunk extends DataChunk<FloatProcessor>
     private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException
     {
         ois.defaultReadObject();
-        processor = new FloatProcessor(width, height, pixels);
+        processor = new FloatProcessor(width, height, pixels, null);
     }
 
     @Override
